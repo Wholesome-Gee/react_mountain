397234407459d0eb76900a403a1d5c76c96198b0
Wholesome-Gee <jiyong0419@naver.com>
2025-04-07 15:40:13 +0900

    배포테스트

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Login from './components/login/Login.jsx'
 import Detail from './components/detail/Detail.jsx'
 
 createRoot(document.getElementById('root')).render(
     <BrowserRouter>
     <Routes>
       <Route path='/' element={<App/>}>
-        <Route path='login' element={<Login/>}/>
         <Route path=':mtnId' element={<Detail/>}/>
       </Route>
     </Routes>
